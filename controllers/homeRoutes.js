d75116a3edf52404024496aa664186cbf936027a
Jakesmitty05 <jakesmitty05@gmail.com>
2024-01-27 21:12:41 -0500

    updated handlebars and added dashboard route

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -49,26 +49,6 @@ router.get('/post/:id', async (req, res) => {
   }
 });
 
-// // Use withAuth middleware to prevent access to route
-// router.get('/profile', withAuth, async (req, res) => {
-//   try {
-//     // Find the logged in user based on the session ID
-//     const userData = await User.findByPk(req.session.user_id, {
-//       attributes: { exclude: ['password'] },
-//       include: [{ model: Post }],
-//     });
-
-//     const user = userData.get({ plain: true });
-
-//     res.render('profile', {
-//       ...user,
-//       logged_in: true
-//     });
-//   } catch (err) {
-//     res.status(500).json(err);
-//   }
-// });
-
 router.get('/login', (req, res) => {
   // If the user is already logged in, redirect the request to another route
   if (req.session.logged_in) {
@@ -80,3 +60,23 @@ router.get('/login', (req, res) => {
 });
 
 module.exports = router;
+
+router.get('/dashboard', withAuth, async (req, res) => {
+  try {
+    const userPosts = await Post.findAll({
+      where: {
+        user_id: req.session.user_id
+      },
+      include: [{ model: User, attributes: ['name'] }]
+    });
+
+    const posts = userPosts.map((post) => post.get({ plain: true }));
+
+    res.render('dashboard', {
+      posts,
+      logged_in: req.session.logged_in
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
